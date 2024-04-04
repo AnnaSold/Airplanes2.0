da1dc9894e8be49866a75ed1d1c2dad12a462f5f
Anna <anutka9602@mail.ru>
2024-04-04 23:20:13 +0300

    реализована работа с объектами

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,42 @@ public class Main {
     public static void main(String[] args) {
         Airplane plane1 = new Airplane("Ту-24", 200);
         plane1.fly();
+
         PistonAircraft planePiston1 = new PistonAircraft("Cessna 340",2);
         planePiston1.makeFastTurn();
+
         CornMackerel planeCorn1 = new CornMackerel("Аннушка", 1);
         planeCorn1.sprayFertilizer();
+
         SportPlane planeSport1 = new SportPlane("Су-26", 1);
         planeSport1.blowSmoke("синего");
 
+        ReactivePlane planeReactive1 = new ReactivePlane("Gloster Meteor", 1);
+        planeReactive1.flyVeryVeryFast();
+
+        BusinessJet planeBusiness1 = new BusinessJet("ИЛ-96",5 );
+        planeBusiness1.transportPeople("Самара");
+
+        Airliner planeAirliner1 = new Airliner("Boing-777", 250);
+        planeAirliner1.transportAlotOfPeople("Уренгой");
+        Airplane[] planes = {planeSport1, planeCorn1, planeBusiness1};
+
+        planeExample(planes);
+
     }
+public static void planeExample(Airplane[] planes){
+       for (Airplane p: planes) p.fly();
+
+       for (Airplane p:planes){
+           if (p instanceof PistonAircraft) {
+               PistonAircraft pp = (PistonAircraft) p;
+               pp.makeFastTurn();
+           }
+           else
+               System.out.println("этот самолёт не поршневой");
+       }
+}
+
+
+
 }
--- src/ReactivePlane.java
+++ src/ReactivePlane.java
@@ -1,2 +1,25 @@
-public class ReactivePlane {
+public class ReactivePlane extends Airplane {
+    public ReactivePlane(String name, int capacity) {
+        super(name, capacity);
+    }
+    public void flyVeryVeryFast(){
+        System.out.println(this.getName() + " летит очень очень быстро");
+    }
+
+}
+class BusinessJet extends Airplane{
+    public BusinessJet(String name, int capacity) {
+        super(name, capacity);
+    }
+    public void transportPeople(String city){
+        System.out.println(this.getName() + " перевозит мало людей в " + city);
+    }
+}
+class Airliner extends Airplane{
+    public Airliner(String name, int capacity) {
+        super(name, capacity);
+    }
+    public void transportAlotOfPeople(String city){
+        System.out.println(this.getName() + " перевозит очень много людей в " + city);
+    }
 }
